fd9cd68be386611a3d2160d52e37098ba30b02d1
alma-mahmud <alma.mahmud.dev@gmail.com>
2026-04-11 00:51:07 +0600

    Testimonial add

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -255,6 +255,21 @@ export const WHY_CHOOSE_US = {
   ],
 };
 
+// TestimonialsSection --------->
+export const REVIEWS = {
+  heading: "Check Our Clients",
+  highlight: "Review",
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.",
+  reviews: [
+    { id: 1, name: "Jenny Wilson",   image: "/images/Review/review1.png", rating: 4, text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." },
+    { id: 2, name: "Esther Howard",  image: "/images/Review/review2.png", rating: 4, text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." },
+    { id: 3, name: "Robert Fox",     image: "/images/Review/review3.png", rating: 4, text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." },
+    { id: 4, name: "Sarah Johnson",  image: "/images/Review/review1.png", rating: 5, text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." },
+    { id: 5, name: "Mark Davis",    image: "/images/Review/review2.png", rating: 4, text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." },
+  ],
+};
+
 // FAQSection --------->
 export const FAQS = [
   {
